ce01647e074de84a647b67e05feb331c4ef72d4e
Scheeto <sam_hudson13@outlook.com>
2025-03-23 18:51:14 -0500

    Add unit test file

---
+++ BingoCardTest.java
@@ -0,0 +1,53 @@
+import org.junit.jupiter.api.Test;
+
+public class BingoCardTest {
+    @Test
+    void testCheckBingo() {
+
+    }
+
+    @Test
+    void testDisplayCard() {
+
+    }
+
+    @Test
+    void testDisplayInUserFriendlyFormat() {
+
+    }
+
+    @Test
+    void testGetCard() {
+
+    }
+
+    @Test
+    void testGetId() {
+
+    }
+
+    @Test
+    void testGetMarked() {
+
+    }
+
+    @Test
+    void testHasNumber() {
+
+    }
+
+    @Test
+    void testHasNumber2() {
+
+    }
+
+    @Test
+    void testIsValid() {
+
+    }
+
+    @Test
+    void testMarkCell() {
+
+    }
+}
