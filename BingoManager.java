d30500319eb5cffcb5db42c5157e59e6d33488a1
Scheeto <sam_hudson13@outlook.com>
2025-03-24 19:44:28 -0500

    Adding Bingo Manager

--- BingoManager.java
+++ BingoManager.java
@@ -1,2 +1,24 @@
-public class BingoManager {
+import java.util.ArrayList;
+import java.util.List;
+
+class BingoManager {
+    private List<Pattern> patterns;
+
+    public BingoManager() {
+        this.patterns = new ArrayList<>();
+    }
+
+    public void addPattern(Pattern pattern) {
+        patterns.add(pattern);
+    }
+
+    public int countBingos(BingoCard card) {
+        int bingoCount = 0;
+        for (Pattern pattern : patterns) {
+            if (pattern.matches(card)) {
+                bingoCount++;
+            }
+        }
+        return bingoCount;
+    }
 }
